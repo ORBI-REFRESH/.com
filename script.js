b0a332bb72825ab1175d0fad79b90966c6a1cc18
ORBI-REFRESH <garciaabreuk@gmail.com>
2025-05-22 18:57:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(" .mySwiper" , {
+    effect: "coverflow",
+    grabCursos: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    convertflowEffect: {
+        rotate:15,
+        strech:0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop: true,
+});
